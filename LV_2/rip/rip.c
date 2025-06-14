3727830935e1d41d62db6eb32621b273d2aa26ef
mouchtach <youssefmouchtach674@gmail.com>
2025-06-14 22:14:02 +0100

    rip v2

--- LV_2/rip/rip.c
+++ LV_2/rip/rip.c
@@ -1,85 +1,93 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-void	min_removals(char *input, size_t inputLen, size_t *rem_l, size_t *rem_r)
-{
-	size_t balance = 0;
-	size_t i = 0;
 
-	while (i < inputLen)
-	{
-		if (input[i] == '(')
-			balance++;
-		else if (input[i] == ')')
-		{
-			if (balance == 0)
-				(*rem_r)++;
-			else
-				balance--;
-		}
-		i++;
-	}
-	*rem_l = balance;
+char *get_var(char *str, int *rm_close, int *rm_open, int *len)
+// had fuction drt buha copy dyal av[1] and check is valid caracter "(" ")" o calcul chhal dyal open o close li khasni ndelete
+{
+    int i = 0;
+    while(str[i])
+    {
+        if(str[i] == '(')
+        {
+            if(*rm_close == 0)
+                (*rm_open)++;
+            else 
+                (*rm_close)--;
+        }
+        else if (str[i] == ')')
+        {
+            if(*rm_open == 0)
+                (*rm_close)++;
+            else
+                (*rm_open)--;
+        }
+        else 
+            return(NULL);
+        i++;
+    }
+    *len = i;
+    char *new = malloc(i + 1);
+    i = 0;
+    while (str[i])
+    {
+       new[i] = str[i];
+       i++;
+    }
+    new[i] = 0;
+    return (new);
 }
 
-void	rip(char *input, size_t i, char *result, size_t rm_open, size_t rm_close, int open)
+
+void    rip(char *str, char *result, int i , int rm_open, int rm_close, int open)
 {
-	// finish
-	if (input[i] == '\0')
-	{
-		if (rm_open == 0 && rm_close == 0 && open == 0)
-		{
-			result[i] = '\0';
-			printf("%s\n", result);
-		}
-		return ;
-	}
-	// not a para
-	if (input[i] != '(' && input[i] != ')')
-	{
-		result[i] = input[i];
-		rip(input, i + 1, result, rm_open, rm_close, open);
-		return ;
-	}
-	// remove
-	if (input[i] == '(' && rm_open > 0)
-	{
-		result[i] = ' ';
-		rip(input, i + 1, result, rm_open - 1, rm_close, open);
-	}
-	if (input[i] == ')' && rm_close > 0)
-	{
-		result[i] = ' ';
-		rip(input, i + 1, result, rm_open, rm_close - 1, open);
-	}
-	// keep
-	if (input[i] == '(')
-	{
-		result[i] = '(';
-		rip(input, i + 1, result, rm_open, rm_close, open + 1);
-	}
-	if (input[i] == ')' && open > 0)
-	{
-		result[i] = ')';
-		rip(input, i + 1, result, rm_open, rm_close, open - 1);
-	}
+    // print
+    if(str[i] == '\0') // hna kan printe result fach hatwsal lhad case
+    {
+        if(rm_close == 0 && rm_open == 0 && open == 0)
+        {
+            result[i] = 0;
+            printf("%s\n", result);
+            return ; 
+        }
+    }
+    // remove
+    if(str[i] == '(' && rm_open > 0)
+    {
+        result[i] = ' ';
+        rip(str, result, i + 1, rm_open - 1, rm_close, open);
+    }
+    if(str[i] == ')' && rm_close > 0)
+    {
+        result[i] = ' ';
+        rip(str, result, i + 1, rm_open , rm_close - 1, open);
+    }
+    // keep
+    if(str[i] == '(')
+    {
+        result[i] = str[i];
+        rip(str, result, i + 1, rm_open , rm_close , open + 1);
+    }
+    if(str[i] == ')' && open > 0)
+    {
+        result[i] = str[i];
+        rip(str, result, i + 1, rm_open , rm_close , open - 1);
+    }
 }
 
-int main(int ac, char *av[])
+int main(int ac, char **av)
 {
-	if (ac != 2)
-		return 1;
-	char *input = av[1];
-	size_t inputLen = strlen(input);
-	char result[255] = {0};
-	// char *result = malloc(strlen(input) + 1);
-	size_t rm_open = 0;
-	size_t rm_close = 0;
-
-	min_removals(input, inputLen, &rm_open, &rm_close);
-
-	rip(input, 0, result, rm_open, rm_close, 0);
-	// free(result);
+    if(ac != 2)
+        return(0);
+    int rm_open = 0;
+    int rm_close = 0;
+    int len = 0;
+    char *str = get_var(av[1], &rm_close, &rm_open, &len);
+    char *result = malloc(len +1 );
+    if(!str)
+        return(1);
+    rip(str, result, 0, rm_open, rm_close, 0);
+    free(str);
+    free(result);
 }
